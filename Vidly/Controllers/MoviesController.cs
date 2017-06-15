33c31f5b366f05f48f99cfc71096cd351667dd8c
Rowell Tores <rtorres22@gmail.com>
2017-06-15 15:44:11 -0700

    Added CustomersController with Views.  Minor changes to logic

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -10,7 +10,15 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
+
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        // GET: Movies/Random
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
@@ -29,11 +37,20 @@ namespace Vidly.Controllers
             return View(viewModel);
         }
 
-        [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1, 12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
+        public IEnumerable<Movie> GetMovies()
         {
-
-            return Content(year + "/" + month);
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-E" }
+            };
         }
+
+        //[Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1, 12)}")]
+        //public ActionResult ByReleaseDate(int year, int month)
+        //{
+
+        //    return Content(year + "/" + month);
+        //}
     }
 }
