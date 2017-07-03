8d5bb5132326cadb146bf7da4cf527e723237cb2
Rowell Tores <rtorres22@gmail.com>
2017-07-02 18:56:16 -0700

    Added new customer form

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -23,6 +23,11 @@ namespace Vidly.Controllers
             _context.Dispose();
         }
 
+        public ActionResult New()
+        {
+            return View();
+        }
+
         public ActionResult Index()
         {
 
